be18baef1a24ffa3aba6c1b92e831866b5d9022d
jongwon0801 <129039041+jongwon0801@users.noreply.github.com>
2025-01-13 17:26:43 +0900

    Create webserver.py

---
+++ kiosk/rasp_setting/webserver.py
@@ -0,0 +1,31 @@
+import tornado.web
+import tornado.ioloop
+import threading
+import elcsoft.controller.websocket
+import wikismartdoor as wikismartdoor
+
+clients = []
+
+if __name__ == "__main__":
+    obj = wikismartdoor.WikiSmartdoor()
+    th = threading.Thread(target=obj.run, daemon=True).start()
+
+    handlers = [
+        (r'/ws', elcsoft.controller.websocket.WebsocketHandler, { 'wikismartdoor': obj, 'clients': clients })
+    ]
+
+    settings = {
+        "cookie_secret": "elcsoft"
+    }
+
+    app = tornado.web.Application(handlers, **settings)
+    app.listen(8080)
+
+    mainLoop = tornado.ioloop.IOLoop.instance()
+
+    try:
+        mainLoop.start()  # mainLoop를 사용하여 서버 시작
+        print("Webserver Started!")
+    except KeyboardInterrupt:
+        mainLoop.stop()  # mainLoop를 사용하여 서버 중지
+        print("Webserver Stopped!")
