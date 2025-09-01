37bcac72a5f6fe66b159db3c1acde78d8a74a4d0
jongwon0801 <129039041+jongwon0801@users.noreply.github.com>
2025-09-01 13:26:21 +0900

    Update (new)pir.py

--- raspberry/신규보드/외부방문/(new)pir.py
+++ raspberry/신규보드/외부방문/(new)pir.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import datetime
 
 # GPIO 설정
 PIR_PIN = 22  # BCM 모드의 GPIO 22번 핀
@@ -9,13 +10,21 @@ GPIO.setup(PIR_PIN, GPIO.IN)  # PIR 센서 입력으로 설정
 def test_pir_sensor():
     try:
         print("PIR 센서 모니터링 시작... (종료하려면 Ctrl+C를 누르세요)")
+        print("시간\t\t\t상태\t\t데이터값")
+        print("-" * 50)
         
         while True:
+            # 현재 시간 가져오기
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            
             # 현재 센서 상태 읽기
-            if GPIO.input(PIR_PIN):
-                print("움직임 감지됨!")
-            else:
-                print("움직임 없음")
+            sensor_value = GPIO.input(PIR_PIN)
+            
+            # 상태 메시지 결정
+            status = "움직임 감지됨!" if sensor_value else "움직임 없음"
+            
+            # 출력
+            print(f"{current_time}\t{status}\t\t{sensor_value}")
             
             time.sleep(0.5)  # 딜레이 약간 늘림 (출력 빈도 조절)
             
